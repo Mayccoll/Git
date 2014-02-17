8b1ea479c8e577bab416bc0b102f7c4e886d1b35
Mayccoll <mayccoll@gmail.com>
2014-02-17 17:57:50 -0500

    Create crontab.sh

---
+++ crontab.sh
@@ -0,0 +1,47 @@
+        ### Cron
+        ###########################
+
+# Pull and log
+*/3 * * * *   /root/crons/prototiposena.sh  >> ~/crons/logs/prototiposena.log 2>&1
+*/3 * * * *   /root/crons/time_stamp.sh  >> ~/crons/logs/git_log_prototiposena.log1 2>&1
+
+
+*/3 * * * *   /root/crons/prototiposena_prueba.sh  >> ~/crons/logs/prototiposena_prueba.log 2>&1
+*/3 * * * *   /root/crons/time_stamp.sh  >> ~/crons/logs/git_log_prototiposena.log2 2>&1
+
+
+
+        ### TimeStamp
+        ###########################
+
+#!/bin/bash
+
+printf "\n"; \
+echo ":::::::::::::::::::::::::"; \
+NOW=$(date +"%Y-%m-%d [%H:%M:%S]"); \
+echo ${NOW}; \
+echo ":::::::::::::::::::::::::"; \
+printf "\n";
+
+
+
+
+        ### Shell
+        ###########################
+
+#!/bin/bash
+
+###################################
+# Repo 
+URL="/var/www/html/prototiposena_prueba"
+
+# From
+PULL_REPO="origin"
+
+# Branch
+BRANCH="Desarrollo"
+
+###################################
+
+cd ${URL} 
+git pull ${PULL_REPO} ${BRANCH}
